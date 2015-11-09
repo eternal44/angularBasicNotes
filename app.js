97b4ec6409ff6f78b37d99987e3509eca6cabb98
James Youn <jamesyoun710@gmail.com>
2015-11-09 10:00:53 -0800

    Use ng-repeat derivative to display index of items

--- app.js
+++ app.js
@@ -2,14 +2,23 @@
   var app = angular.module('store', []);
 
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Dodecachedron',
-    price: 2.95,
-    description: '...',
-    canPurchase: false,
-    soldOut: false
-  }
+  var gems = [
+    {
+      name: 'Dodecachedron',
+      price: 2.95,
+      description: '...',
+      canPurchase: false,
+      soldOut: false
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 3.95,
+      description: 'Bigger Gem!',
+      canPurchase: false,
+      soldOut: true
+    }
+  ]
 })();
