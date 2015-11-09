0816d99214c486224598d5660e82a255bda79c8b
James Youn <jamesyoun710@gmail.com>
2015-11-09 10:59:09 -0800

    Use ng-src directive

--- app.js
+++ app.js
@@ -11,14 +11,24 @@
       price: 2.000,
       description: '...',
       canPurchase: false,
-      soldOut: false
+      soldOut: false,
+      images: [
+        "images/gem-01.gif",
+        "images/gem-03.gif",
+        "images/gem-04.gif"
+      ],
     },
     {
       name: 'Pentagonal Gem',
       price: 3.95,
       description: 'Bigger Gem!',
       canPurchase: false,
-      soldOut: true
+      soldOut: true,
+      images: [
+        "images/gem-05.gif",
+        "images/gem-06.gif",
+        "images/gem-07.gif"
+      ],
     }
   ]
 })();
