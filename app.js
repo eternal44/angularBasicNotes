a3e2151dd8e35160aa3d178a1752620efa804530
James Youn <jamesyoun710@gmail.com>
2015-11-11 11:04:41 -0800

    Extract product directives into it's own module
    Refactoring to organize code cluttler

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('store', []);
+  var app = angular.module('store', ['store-products']);
 
   app.controller('StoreController', function() {
     this.products = gems;
@@ -12,46 +12,6 @@
       product.reviews.push(this.review);
       this.review = {};
     };
-  });
-
-  app.directive('productPanels', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-panels.html',
-      controller: function() {
-        this.tab = 1;
-
-        this.selectTab = function(setTab) {
-          this.tab = setTab
-        };
-
-        this.isSelected = function(checkTab){
-          return this.tab === checkTab
-        };
-      },
-      controllerAs: 'panel'
-    };
-  });
-
-  app.directive('productSpecs', function() {
-    return {
-      restrict: 'A',
-      templateUrl: 'product-specs.html'
-    };
-  });
-
-  app.directive('productTitle', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-title.html'
-    };
-  });
-
-  app.directive('productDescription', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-description.html'
-    }
   });
 
   var gems = [
